186bfcf3b12efa6296ae3b4184981c368562603b
localrice <borokinjalkavish@gmail.com>
2025-08-04 00:46:50 +0530

    added padding to accomodate for the navbar

--- src/sites/Team/TeamPage.jsx
+++ src/sites/Team/TeamPage.jsx
@@ -1,10 +1,11 @@
 import { TeamSection } from "@/components/team";
 import { Navbar, Footer } from "@/components/globals";
+import "./TeamPage.scss";
 export default function TeamPage() {
     return (
         <>
             <Navbar />
-            <main>
+            <main className="team-section">
                 <TeamSection />
             </main>
             <Footer />
